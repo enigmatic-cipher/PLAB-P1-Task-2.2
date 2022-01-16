0d663e394e26255629159cda440147edf30c9fde
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-16 09:40:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Refer code from Task 2.1. Set value of n according to following table. Now RUN your code and check if your output matches with Expected Output.
+
+n = 3
+for i in range(1,n+1):
+  print(i)
